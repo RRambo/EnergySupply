5151c8f54191ebb23d8143ad04f867e8ac0bd106
Rami <rami.kontio@hotmail.fi>
2026-04-14 15:58:21 +0300

    Started creating house component

---
+++ frontend/src/components/house.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './house.css'
+
+function houseCard(residents) {
+    const [powerGen, setPowerGen] = useState(0.0)
+    const [powerConsumption, setPowerConsumption] = useState(0.0)
+    const [residents, setResidents] = useState(0)
+    const [optIn, setOptIn] = useState(false)
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default houseCard
