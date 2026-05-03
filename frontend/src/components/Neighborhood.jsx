e22efc8c5bf22650273e784050f2f27b7353fe46
Rami <rami.kontio@hotmail.fi>
2026-05-03 15:46:45 +0300

    fix: (responsiveness) fixed house sizes not scaling correctly in different size viewports

--- frontend/src/components/Neighborhood.jsx
+++ frontend/src/components/Neighborhood.jsx
@@ -152,15 +152,16 @@ function Neighborhood() {
                             left: `${left}%`,
                             top: `${top}%`,
                             pointerEvents: 'auto',
+                            width: `${shape.widthPercent}%`,
+                            height: `${shape.heightPercent}%`,
+                            // CAN'T add the rotation here!!!
                             opacity: 1 // Helpful for getting the house positions and shapes correct
+                            // (NOTE!, only change the opacity to < 0 for the duration of modifying the shapes, as it will cause other problems)
                         }}
                     >
                         <House
                             houseNumber={id}
                             shape={shape}
-                            houseWidth={shape.width}
-                            houseHeight={shape.height}
-                            houseRotation={`rotate(${shape.rotation}deg)`}
 
                             householdData={singleHouseholdData}
                             householdPowerData={singleHouseholdPowerData}
@@ -170,16 +171,16 @@ function Neighborhood() {
             })}
             <div className='road'>
                 {/* road made with: https://editsvgcode.com/ */}
-                <svg viewBox='0 0 800 390' fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <g opacity="1">
-                        <rect x="3" y="324.167" width="793.489" height="28.6948" rx="14.3474" transform="rotate(2.31639 1.15973 273.167)" fill="#d4d4d4" />
-                        <rect x="222.055" y="19.3645" width="502.935" height="30.8319" rx="15.4159" transform="rotate(-2.2066 222.055 19.3645)" fill="#d4d4d4" />
-                        <rect x="162.701" y="299.665" width="331.793" height="32.4859" rx="16.243" transform="rotate(-87.9809 212.701 299.665)" fill="#d4d4d4" />
-                        <rect x="519.72" y="315.212" width="346.968" height="34.0875" rx="17.0438" transform="rotate(-90.8936 559.72 325.212)" fill="#d4d4d4" />
+                <svg viewBox='0 0 800 390' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                    <g opacity='1'>
+                        <rect x='3' y='324.167' width='793.489' height='28.6948' rx='14.3474' transform='rotate(2.31639 1.15973 273.167)' fill='#d4d4d4' />
+                        <rect x='222.055' y='19.3645' width='502.935' height='30.8319' rx='15.4159' transform='rotate(-2.2066 222.055 19.3645)' fill='#d4d4d4' />
+                        <rect x='162.701' y='299.665' width='331.793' height='32.4859' rx='16.243' transform='rotate(-87.9809 212.701 299.665)' fill='#d4d4d4' />
+                        <rect x='519.72' y='315.212' width='346.968' height='34.0875' rx='17.0438' transform='rotate(-90.8936 559.72 325.212)' fill='#d4d4d4' />
                     </g>
                 </svg>
             </div>
-            {error && <p className="card-error">{error}</p>}
+            {error && <p className='card-error'>{error}</p>}
         </>
     )
 }
